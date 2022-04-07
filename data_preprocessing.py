23e384cab1c62c7fb431dcbde024fe0b348bc2a9
faiazrahman <faiaz.rahman@yale.edu>
2022-04-07 14:39:26 -0400

    preprocess dialogue data, summ pipeline, update BART model

--- data_preprocessing.py
+++ data_preprocessing.py
@@ -22,14 +22,15 @@ IMAGES_DIR = os.path.join(DATA_PATH, "images")
 TRAIN_DATA_SIZE = 10000
 TEST_DATA_SIZE = 1000
 
-logging.basicConfig(level=logging.INFO) # DEBUG, INFO, WARNING, ERROR, CRITICAL
+logging.basicConfig(level=logging.DEBUG) # DEBUG, INFO, WARNING, ERROR, CRITICAL
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--train", action="store_true", help="Running on training data")
     parser.add_argument("--test", action="store_true", help="Running on test (evaluation) data")
     parser.add_argument("--from_dialogue_dataframe", type=str, default=None, help="If you're using dialogue (comment) data and have already filtered the dialogue dataframe, pass the path to its serialized .pkl file to continue preprocessing from that point on")
-    parser.add_argument("--dir_to_save_dataframe", type=str, default="data", help="Path to store saved dataframe .pkl file after data preprocessing")
+    parser.add_argument("--dir_to_save_dataframe", type=str, default="data/Fakeddit", help="Path to store saved dataframe .pkl file after data preprocessing")
+    parser.add_argument("--modality", type=str, default=None, help="text | image | text-image | text-image-dialogue")
     parser.add_argument("--config", type=str, default="", help="config.yaml file with experiment configuration")
     args = parser.parse_args()
 
@@ -40,11 +41,11 @@ if __name__ == "__main__":
 
     args.train_data_path = config.get("train_data_path", os.path.join(DATA_PATH, "multimodal_train_" + str(TRAIN_DATA_SIZE) + ".tsv"))
     args.test_data_path = config.get("test_data_path", os.path.join(DATA_PATH, "multimodal_test_" + str(TEST_DATA_SIZE) + ".tsv"))
-    args.modality = config.get("modality", "text-image")
+    if not args.modality: args.modality = config.get("modality", "text-image")
     args.num_classes = config.get("num_classes", 2)
     args.text_embedder = config.get("text_embedder", "all-mpnet-base-v2")
     args.image_encoder = config.get("image_encoder", "resnet")
-    args.dialogue_summarization_model = config.get("dialogue_summarization_model", "bart-large-cnn")
+    args.dialogue_summarization_model = config.get("dialogue_summarization_model", "facebook/bart-large-cnn")
     logging.info(args)
 
     # text_embedder = SentenceTransformer(args.text_embedder)
--- dataloader.py
+++ dataloader.py
@@ -21,6 +21,7 @@ logging.basicConfig(level=logging.DEBUG) # DEBUG, INFO, WARNING, ERROR, CRITICAL
 DATA_PATH = "./data/Fakeddit"
 PL_ASSETS_PATH = "./lightning_logs"
 IMAGES_DIR = os.path.join(DATA_PATH, "images")
+DIALOGUE_DATA_FILE = os.path.join(DATA_PATH, "all_comments.tsv")
 IMAGE_EXTENSION = ".jpg"
 
 class Modality(enum.Enum):
@@ -72,6 +73,20 @@ class MultimodalDataset(Dataset):
         elif Modality(modality) == Modality.TEXT_IMAGE_DIALOGUE:
             self.saved_dataframe_filename_prefix = "text_image_dialogue"
 
+        self.text_embedder = text_embedder
+        self.image_transform = image_transform
+        self.summarization_model = summarization_model
+
+        # TODO: Handle in-house summarization model
+        self.summarizer = None
+        if Modality(modality) == Modality.TEXT_IMAGE_DIALOGUE and summarization_model:
+            # Model options: "facebook/bart-large-cnn", "t5-small", "t5-base", "t5-large", "t5-3b", "t5-11b"
+            # https://huggingface.co/docs/transformers/master/en/main_classes/pipelines#transformers.SummarizationPipeline
+            self.summarizer = transformers.pipeline("summarization", model=summarization_model)
+        elif Modality(modality) == Modality.TEXT_IMAGE_DIALOGUE:
+            self.summarizer = transformers.pipeline("summarization")
+
+
         df = None
         if not from_preprocessed_dataframe:
             print("Running data preprocessing from scratch...")
@@ -80,6 +95,10 @@ class MultimodalDataset(Dataset):
             df = self._preprocess_df(df)
             logging.debug(df.columns)
             logging.debug(df['clean_title'])
+
+            # Save main dataframe and ids (needed for dialogue preprocessing)
+            self.data_frame = df
+            self.text_ids = set(self.data_frame['id'])
 
             # Run dialogue preprocessing, if needed
             if Modality(modality) == Modality.TEXT_IMAGE_DIALOGUE:
@@ -92,6 +111,9 @@ class MultimodalDataset(Dataset):
                     self._preprocess_dialogue(from_saved_df_path=from_dialogue_dataframe)
                 else:
                     self._preprocess_dialogue()
+
+            # TODO: Save final dataframe
+
         else: # from_preprocessed_dataframe:
             print("Running data preprocessing from preprocessed dataframe...")
             df = None
@@ -107,18 +129,7 @@ class MultimodalDataset(Dataset):
         self.text_ids = set(self.data_frame['id'])
         logging.debug(self.data_frame)
 
-        self.text_embedder = text_embedder
-        self.image_transform = image_transform
-        self.summarization_model = summarization_model
-
-        # TODO: Handle in-house summarization model
-        # self.summarizer = None
-        # if Modality(modality) == Modality.TEXT_IMAGE_DIALOGUE and summarization_model:
-        #     # Model options: "bart-large-cnn", "t5-small", "t5-base", "t5-large", "t5-3b", "t5-11b"
-        #     # https://huggingface.co/docs/transformers/master/en/main_classes/pipelines#transformers.SummarizationPipeline
-        #     self.summarizer = transformers.pipeline("summarization", model=summarization_model)
-        # elif Modality(modality) == Modality.TEXT_IMAGE_DIALOGUE:
-        #     self.summarizer = transformers.pipeline("summarization")
+        return
 
     def __len__(self):
         return len(self.data_frame.index)
@@ -160,4 +171,97 @@ class MultimodalDataset(Dataset):
         return df
 
     def _preprocess_dialogue(self, from_saved_df_path=""):
-        pass
+        """
+        Preprocesses dialogue data by generating dialogue summaries and storing
+        them as a column in the main dataframe
+
+        A comment's 'submission_id' is linked (i.e. equal) to a post's 'id';
+        'body' contains the comment text and 'ups' contains upvotes
+        """
+
+        # Save this dialogue dataframe into a pickle
+        # Filename will look something like "train__text_image_dialogue__dialogue_dataframe.pkl"
+        filename = "__".join([self.dataset_type, self.saved_dataframe_filename_prefix, "dialogue_dataframe.pkl"])
+        save_path = os.path.join(self.dir_to_save_dataframe, filename)
+
+        def generate_summaries_and_save_df(df, save_path="data"):
+            """ df: Dataframe made from dialogue data file """
+            # Add new column in main dataframe to hold dialogue summaries
+            self.data_frame['comment_summary'] = ""
+
+            failed_ids = []
+            for iteration, text_id in enumerate(self.text_ids):
+                if (iteration % 250 == 0):
+                    print("Generating summaries for item {}...".format(iteration))
+                    # Save progress so far
+                    self.data_frame.to_pickle(save_path)
+
+                try:
+                    # Group comments by post id
+                    all_comments = df[df['submission_id'] == text_id]
+                    all_comments.sort_values(by=['ups'], ascending=False)
+                    all_comments = list(all_comments['body'])
+
+                    # Generate summary of comments via Transformers pipeline
+                    corpus = "\n".join(all_comments)
+                    summary = "none" # Default if no comments for this post
+                    if len(all_comments) > 0:
+                        # We define the summary's max_length as max(min(75, num_words // 2), 5)
+                        # Note that num_words is calculated very roughly, splitting on whitespace
+                        num_words = sum([len(comment.split()) for comment in all_comments])
+                        max_length = min(75, num_words // 2) # For short comment threads, it'll be <75
+                        max_length = max(max_length, 5) # Avoid 1-length maxes, which leads to unexpected behavior
+                        min_length = min(5, max_length - 1)
+                        summary = self.summarizer(corpus, min_length=min_length, max_length=max_length, truncation=True)
+
+                        # Pipeline returns a list containing a dict
+                        # https://huggingface.co/docs/transformers/master/en/main_classes/pipelines
+                        summary = summary[0]['summary_text']
+
+                    # Add summary to self.data_frame 'comment_summary' column
+                    self.data_frame.loc[self.data_frame['id'] == text_id, 'comment_summary'] = summary
+                except:
+                    failed_ids.append(text_id)
+
+            # Save final dialogue dataframe
+            self.data_frame.to_pickle(save_path)
+            print("Preprocessed dialogue dataframe saved to {}".format(save_path))
+            logging.info("Preprocessed dialogue dataframe saved to {}".format(save_path))
+
+            logging.debug(self.data_frame)
+            logging.debug(self.data_frame['comment_summary'])
+            logging.debug("num_failed:", len(failed_ids))
+            logging.debug(failed_ids)
+
+            return
+
+        if from_saved_df_path != "":
+            # Special Case (see above comment in __init__)
+            df = pd.read_pickle(from_saved_df_path)
+            generate_summaries_and_save_df(df, save_path=save_path)
+        else:
+            df = pd.read_csv(DIALOGUE_DATA_FILE, sep='\t')
+            logging.debug(df)
+
+            def text_exists(row):
+                """ Ensures that a comment's corresponding text exists """
+                if row['submission_id'] in self.text_ids:
+                    return True
+                else:
+                    return False
+
+            def comment_deleted(row):
+                """ If a comment was deleted, its body just contains [deleted] """
+                return row['body'] == "[deleted]"
+
+            df['text_exists'] = df.apply(lambda row: text_exists(row), axis=1)
+            df = df[df['text_exists'] == True].drop('text_exists', axis=1)
+            df['comment_deleted'] = df.apply(lambda row: comment_deleted(row), axis=1)
+            df = df[df['comment_deleted'] == False].drop('comment_deleted', axis=1)
+            df.reset_index(drop=True, inplace=True)
+            logging.debug(df)
+
+            # Save dataframe so far before summary generation
+            df.to_pickle(save_path)
+
+            generate_summaries_and_save_df(df, save_path=save_path)

8b42761efb9b404ae8af3b714872a0117521646e
faiazrahman <faiaz.rahman@yale.edu>
2022-04-15 14:00:10 -0400

    add experiment configs for text-image and text-image-dialogue with mpnet,roberta-resnet-bart, automate experiments and logging

--- run_experiments.py
+++ run_experiments.py
@@ -26,4 +26,16 @@ def run(command: str):
 if __name__ == "__main__":
 
     # subprocess.call("python run_training.py --config configs/sampled_text_image_dialogue__2_class__mpnet_resnet_bart.yaml", shell=True)
-    run("python run_training.py --only_check_args --config configs/sampled_text_image_dialogue__2_class__mpnet_resnet_bart.yaml")
+    # run("python run_training.py --only_check_args --config configs/sampled_text_image_dialogue__2_class__mpnet_resnet_bart.yaml")
+
+    run("python run_training.py --config configs/text_image__3_class__mpnet_resnet.yaml")
+    run("python run_training.py --config configs/text_image__6_class__mpnet_resnet.yaml")
+    run("python run_training.py --config configs/text_image_dialogue__3_class__mpnet_resnet_bart.yaml")
+    run("python run_training.py --config configs/text_image_dialogue__6_class__mpnet_resnet_bart.yaml")
+    run("python run_training.py --config configs/text_image__2_class__roberta_resnet.yaml")
+    run("python run_training.py --config configs/text_image__3_class__roberta_resnet.yaml")
+    run("python run_training.py --config configs/text_image__6_class__roberta_resnet.yaml")
+    run("python run_training.py --config configs/text_image_dialogue__2_class__roberta_resnet_bart.yaml")
+    run("python run_training.py --config configs/text_image_dialogue__3_class__roberta_resnet_bart.yaml")
+    run("python run_training.py --config configs/text_image_dialogue__6_class__roberta_resnet_bart.yaml")
+

fcceb2997ea53fb7c2703c70b2cebffd4131fefe
faiazrahman <faiaz.rahman@yale.edu>
2022-04-17 13:30:32 -0400

    init argument_graphs/, view dialogues

---
+++ argument_graphs/view_dialogues.py
@@ -0,0 +1,53 @@
+import os
+import logging
+import argparse
+from pprint import pprint
+
+import pandas as pd
+
+DATA_PATH = "../data/Fakeddit"
+# TRAIN_PREPROCESSED_DATAFRAME_PATH = os.path.join(DATA_PATH, "train__text_image_dialogue__dataframe.pkl")
+# TEST_PREPROCESSED_DATAFRAME_PATH = os.path.join(DATA_PATH, "test__text_image_dialogue__dataframe.pkl")
+# TRAIN_DIALOGUE_DATAFRAME_PATH = os.path.join(DATA_PATH, "train__dialogue_dataframe.pkl")
+# TEST_DIALOGUE_DATAFRAME_PATH = os.path.join(DATA_PATH, "test__dialogue_dataframe.pkl")
+TRAIN_PREPROCESSED_DATAFRAME_PATH = os.path.join(DATA_PATH, "sampled_train__text_image_dialogue__dataframe.pkl")
+TEST_PREPROCESSED_DATAFRAME_PATH = os.path.join(DATA_PATH, "sampled_test__text_image_dialogue__dataframe.pkl")
+TRAIN_DIALOGUE_DATAFRAME_PATH = os.path.join(DATA_PATH, "sampled_train__dialogue_dataframe.pkl")
+TEST_DIALOGUE_DATAFRAME_PATH = os.path.join(DATA_PATH, "sampled_test__dialogue_dataframe.pkl")
+
+logging.basicConfig(level=logging.DEBUG) # DEBUG, INFO, WARNING, ERROR, CRITICAL
+
+def display_items_with_dialogue(df_path: str):
+    """ Takes in preprocessed dataframe path """
+
+    def has_dialogue(row):
+        return row['comment_summary'] != "none"
+
+    df = pd.read_pickle(df_path)
+    df['has_dialogue'] = df.apply(lambda row: has_dialogue(row), axis=1)
+    df = df[df['has_dialogue'] == True].drop('has_dialogue', axis=1)
+    print(df)
+    print(df.columns)
+
+def view_dialogues(df_path: str):
+    """ Takes in dialogue dataframe path """
+    df = pd.read_pickle(df_path)
+    print(df)
+    print(df.columns)
+    text_ids = df['submission_id'].unique()
+
+    for indx, text_id in enumerate(text_ids):
+        # Group comments by post id
+        all_comments = df[df['submission_id'] == text_id]
+        all_comments.sort_values(by=['ups'], ascending=False)
+        all_comments = list(all_comments['body'])
+        pprint(all_comments)
+
+        if indx > 10: break
+
+if __name__ == "__main__":
+    display_items_with_dialogue(TRAIN_PREPROCESSED_DATAFRAME_PATH)
+    display_items_with_dialogue(TEST_PREPROCESSED_DATAFRAME_PATH)
+
+    view_dialogues(TRAIN_DIALOGUE_DATAFRAME_PATH)
+    view_dialogues(TEST_DIALOGUE_DATAFRAME_PATH)

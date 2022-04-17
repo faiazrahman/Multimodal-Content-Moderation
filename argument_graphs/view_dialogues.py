2ae3352e7bbd9dcf00fd3076656097e73e38ed06
faiazrahman <faiaz.rahman@yale.edu>
2022-04-17 15:46:35 -0400

    simple script to view dialogues

--- argument_graphs/view_dialogues.py
+++ argument_graphs/view_dialogues.py
@@ -1,7 +1,7 @@
 import os
 import logging
 import argparse
-from pprint import pprint
+from typing import List
 
 import pandas as pd
 
@@ -16,6 +16,10 @@ TRAIN_DIALOGUE_DATAFRAME_PATH = os.path.join(DATA_PATH, "sampled_train__dialogue
 TEST_DIALOGUE_DATAFRAME_PATH = os.path.join(DATA_PATH, "sampled_test__dialogue_dataframe.pkl")
 
 logging.basicConfig(level=logging.DEBUG) # DEBUG, INFO, WARNING, ERROR, CRITICAL
+
+def print_list(lst: List[str]):
+    for element in lst:
+        print(">" + element)
 
 def display_items_with_dialogue(df_path: str):
     """ Takes in preprocessed dataframe path """
@@ -41,9 +45,13 @@ def view_dialogues(df_path: str):
         all_comments = df[df['submission_id'] == text_id]
         all_comments.sort_values(by=['ups'], ascending=False)
         all_comments = list(all_comments['body'])
-        pprint(all_comments)
 
-        if indx > 10: break
+        print("=" * 80)
+        print(f"post id:{text_id}, {len(all_comments)} comments\n")
+        print_list(all_comments)
+        print("=" * 80)
+
+        if indx > 20: break
 
 if __name__ == "__main__":
     display_items_with_dialogue(TRAIN_PREPROCESSED_DATAFRAME_PATH)

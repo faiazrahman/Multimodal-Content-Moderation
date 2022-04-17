5370a25c7af65c3f2fcae78622c798f544dd0bc1
faiazrahman <faiaz.rahman@yale.edu>
2022-04-17 18:30:54 -0400

    docstrings for argument_graphs/data_preprocessing.py

--- argument_graphs/data_preprocessing.py
+++ argument_graphs/data_preprocessing.py
@@ -3,8 +3,33 @@ import os
 import logging
 import argparse
 
-AMPERSAND_DATA_PATH = "../data/AMPERSAND"
-SGAM_DATA_PATH = "../data/StabGurevychArgMining"
+DATA_PATH = "../data"
+AMPERSAND_DATA_PATH = os.path.join(DATA_PATH, "AMPERSAND")
+SGAM_DATA_PATH = os.path.join(DATA_PATH, "StabGurevychArgMining")
+AUC_DATA_PATH = os.path.join(DATA_PATH, "ArgumentativeUnitClassification")
+
+# Original data files for AMPERSAND
+AMPERSAND_ORIGINAL_TRAIN_DATA_PATH = os.path.join(AMPERSAND_DATA_PATH, "claimtrain.tsv")
+AMPERSAND_ORIGINAL_DEV_DATA_PATH = os.path.join(AMPERSAND_DATA_PATH, "claimdev.tsv")
+
+# Directory containing all original "*.ann" files for Stab & Gurevych data
+SGAM_ORIGINAL_DATA_DIR = os.path.join(SGAM_DATA_PATH, "annotated_data")
+
+# AMPERSAND data after `clean_ampersand_data()`
+AMPERSAND_AUC_DATA_FILE = "ampersand_auc_data.tsv" # ['text', 'label']
+AMPERSAND_AUC_DATA_PATH = os.path.join(AMPERSAND_DATA_PATH, AMPERSAND_AUC_DATA_FILE)
+
+# Stab & Gurevych data after `clean_and_combine_sgam_data()`
+SGAM_AUC_DATA_FILE = "sgam_auc_data.tsv" # ['text', 'label']
+SGAM_AUC_DATA_PATH = os.path.join(SGAM_DATA_PATH, SGAM_AUC_DATA_FILE)
+
+# Combined AMPERSAND and Stab & Gurevych data -> ['text', 'label']
+# Our 'text' is the argumentative unit (e.g. a sentence)
+# Our 'label' is one of 0: Non-argumentative data, 1: Claim, 2: Premise
+ALL_AUC_DATA_FILE = "all_auc_data.tsv"
+ALL_AUC_DATA_PATH = os.path.join(AUC_DATA_PATH, ALL_AUC_DATA_FILE)
+ALL_AUC_DATAFRAME_FILE = "auc_dataframe.pkl"
+ALL_AUC_DATAFRAME_PATH = os.path.join(AUC_DATA_PATH, ALL_AUC_DATAFRAME_FILE)
 
 def clean_ampersand_data():
     """
@@ -16,7 +41,16 @@ def clean_ampersand_data():
     Note: We combine the original train and dev since we will split the data
     later after it is combined with the Stab & Gurevych data
 
-    Creates `ampersand_auc_data.tsv` in `data/AMPERSAND/`
+    Creates `ampersand_auc_data.tsv` in `data/AMPERSAND/`, which has two
+    columns ['text', 'label']
+
+    Notes about original data format
+    - .tsv with two columns: [text, label]
+    - Labels are as follows
+        0   Non-argumentative unit
+        1   Claim
+        2   Premise
+    - The created `ampersand_auc_data.tsv` has these same labels
     """
     pass
 
@@ -27,7 +61,24 @@ def clean_and_combine_sgam_data():
     classification, and stores it in a single .tsv for easy loading into a
     pandas DataFrame later
 
-    Creates `sgam_auc_data.tsv` in `data/StabGurevychArgMining/`
+    Creates `sgam_auc_data.tsv` in `data/StabGurevychArgMining/`, which has
+    two columns ['text', 'label']
+
+    Notes about original data format
+    - The annotated data is in the `*.ann` files, with one file per original
+      document; note that this is a tab-separated text file (like a .tsv), where the second column is the label and the fifth column is the argumentative unit (i.e. sentence).
+    - Labels are strings, as follows
+        Claim
+        MajorClaim
+        Premise
+    - Note that the annotated data has other labels, like "Stance", "supports",
+      and so on. However, we will only be using the above three labels since
+      we are using this data (along with AMPERSAND) to train an argumentative
+      unit classification model.
+    - Additionally, we will assign both "Claim" and "MajorClaim" to the label
+      1 (for claim), and "Premise" to the label 2 (for premise), matching the
+      AMPERSAND data. (Note that the AMPERSAND data also has the label 0 for
+      non-argumentative units.)
     """
     pass
 

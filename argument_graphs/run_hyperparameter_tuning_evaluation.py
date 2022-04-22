5f5e7b11e04948e8911a9c06fa10e9c0fb94fe16
faiazrahman <faiaz.rahman@yale.edu>
2022-04-22 09:12:06 -0400

    trained model versions for RTC bert,adam,5e-5 and bert,adam,3e-5

--- argument_graphs/run_hyperparameter_tuning_evaluation.py
+++ argument_graphs/run_hyperparameter_tuning_evaluation.py
@@ -9,7 +9,7 @@ import itertools
 
 from run_experiments import run
 
-GPU_ID = 1
+GPU_ID = 3
 
 def evaluation_auc():
     # Hyperparameter tuning for AUC created models 145 to 172, inclusive
@@ -46,17 +46,19 @@ def evaluation_auc_preshuffled():
         print(f"Evaluating model version_{version_number}")
         run(f"python -m argument_graphs.run_argument_graph_submodel_evaluation --argumentative_unit_classification --config configs/argumentative_unit_classification/auc__roberta-base.yaml --batch_size 16 --trained_model_version {version_number} --gpus {GPU_ID}")
 
-def evaluation_rtc_first_three():
-    model_version_numbers = [
-        173, # bert, adam, 1e-3
-        259, # bert, adam, 5e-4
-        229, # bert, adam, 3e-4
+def evaluation_rtc():
+    bert_model_version_numbers = [
+        # 173, # bert, adam, 1e-3
+        # 259, # bert, adam, 5e-4
+        # 229, # bert, adam, 3e-4
+        243, # bert, adam, 5e-5
+        263, # bert, adam, 3e-5
     ]
-    for version_number in model_version_numbers:
+    for version_number in bert_model_version_numbers:
         print(f"Evaluating model version_{version_number}")
         run(f"python -m argument_graphs.run_argument_graph_submodel_evaluation --relationship_type_classification --config configs/relationship_type_classification/rtc__bert-base-uncased.yaml --batch_size 32 --trained_model_version {version_number} --gpus {GPU_ID}")
 
 if __name__ == "__main__":
     # evaluation_auc()
     # evaluation_auc_preshuffled()
-    evaluation_rtc_first_three()
+    evaluation_rtc()

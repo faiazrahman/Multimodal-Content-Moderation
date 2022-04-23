950badc44c81cc4a30f6872166ba5832936b6baa
faiazrahman <faiaz.rahman@yale.edu>
2022-04-23 18:29:54 -0400

    source-to-targets entailment staticmethod with batching, run premise-to-claim entailment

--- argument_graphs/modules/graph_construction.py
+++ argument_graphs/modules/graph_construction.py
@@ -1,6 +1,7 @@
 import logging
-from typing import List
+from typing import List, Tuple
 
+import transformers
 from transformers import AutoTokenizer
 
 from utils import get_checkpoint_path_from_trained_model_version
@@ -14,6 +15,8 @@ from .utterance_segmentation import UtteranceToArgumentativeUnitSegmenter
 
 logging.basicConfig(level=logging.INFO)
 
+ENTAILMENT_SCORE_MINIMUM_THRESHOLD = 0.2
+
 class ArgumentGraphConstructor:
 
     def __init__(
@@ -25,10 +28,12 @@ class ArgumentGraphConstructor:
         rtc_tokenizer_model_name: str = "bert-base-uncased",
         auc_model_batch_size: int = 16,
         rtc_model_batch_size: int = 32,
+        entailment_score_minimum_threshold: float = ENTAILMENT_SCORE_MINIMUM_THRESHOLD,
     ):
         logging.info("Initializing ArgumentGraphConstructor instance...")
         logging.info("NOTE: Make sure that the auc_tokenizer_model_name matches the base model of the given auc_trained_model_version (and the same for rtc_*)")
         logging.info("> You can check what base model was used to train each model by checking hparams.yaml files in the lightning_logs/ folders for your trained models")
+
         if not auc_trained_model_version or not rtc_trained_model_version:
             raise ValueError("ArgumentGraphConstructor must be passed both an auc_trained_model_version and a rtc_trained_model_version")
 
@@ -47,6 +52,7 @@ class ArgumentGraphConstructor:
 
         self.auc_model_batch_size = auc_model_batch_size
         self.rtc_model_batch_size = rtc_model_batch_size
+        self.entailment_score_minimum_threshold = entailment_score_minimum_threshold
 
     def construct_graph(self, dialogue_utterances: List[str]) -> ArgumentGraph:
         # Utterance segmentation
@@ -88,8 +94,96 @@ class ArgumentGraphConstructor:
                 all_nodes.append(node)
                 all_nodes_by_type[classification].append(node)
 
+        # ::TESTING
         for node_type in all_nodes_by_type.keys():
             print(str(node_type))
             for node in all_nodes_by_type[node_type]:
                 print(node.text)
             print("")
+        # ::END
+
+        # Instantiate an ArgumentGraph object
+        graph = ArgumentGraph()
+
+        # Premise-to-claim entailment
+        for premise in all_nodes_by_type[ArgumentativeUnitType.PREMISE]:
+            all_claims = all_nodes_by_type[ArgumentativeUnitType.CLAIM]
+            max_claim, max_probability = ArgumentGraphConstructor.source_to_targets_entailment(
+                source=premise,
+                targets=all_claims,
+                tokenizer=self.rtc_tokenizer,
+                rtc_model=self.rtc_model,
+                batch_size=self.rtc_model_batch_size
+            )
+
+            if max_probability and max_probability > self.entailment_score_minimum_threshold:
+                edge = RelationshipTypeEdge(
+                    source_node=premise,
+                    target_node=max_claim,
+                    relation=RelationshipType.SUPPORTS
+                )
+                graph.add_edge(edge)
+
+        graph.print_graph()
+
+    @staticmethod
+    def source_to_targets_entailment(
+        source: ArgumentativeUnitNode = None,
+        targets: List[ArgumentativeUnitNode] = None,
+        tokenizer: transformers.AutoTokenizer = None,
+        rtc_model: RelationshipTypeClassificationModel = None,
+        batch_size: int = 32,
+    ) -> Tuple[ArgumentativeUnitNode, int]:
+        """
+        Computes entailment score for given `source` against all other
+        `targets` (checking that the other `targets` do not include the given
+        `source`, i.e. no self-loops in the graph) and returns the node in
+        `targets` which the given `source` entails with the highest score,
+        and that score
+
+        Returns None, None if there are no entailment (SUPPORTS) relationships
+        between the source and all the targets
+        """
+
+        # If the `source` is in `targets`, remove it
+        targets = [target for target in targets if target != source]
+
+        # Create batches of targets (so we can pass more through the RTC model
+        # simultaneously, increasing efficiency)
+        targets_batches = generate_batches(targets, batch_size=batch_size)
+
+        max_probability = None
+        max_target = None
+        for targets_batch in targets_batches:
+            # Extract the text only
+            targets_batch_text = [target.text for target in targets_batch]
+
+            # Create a batch of the source text repeated, matching the size of
+            # the targets batch; this is because we are passing the same source
+            # every time to the RTC model (with every possible target)
+            # e.g. source_batch = ["a", "a", "a", "a", "a", "a"]
+            #      target_batch = ["b", "c", "d", "e", "f", "g"]
+            source_batch_text = [source.text for _ in range(len(targets_batch))]
+
+            encoded_inputs = encode_paired_inputs(
+                source_batch_text,
+                targets_batch_text,
+                tokenizer=tokenizer
+            )
+            preds, losses = rtc_model(encoded_inputs) # TODO: Return probabilities too
+            probabilities = [1.0 for _ in range(len(targets_batch))] # TODO: Get probabilities from model
+            print(preds)
+
+            for i, target in enumerate(targets_batch):
+                if (preds[i] == int(RelationshipType.SUPPORTS)
+                    and (not max_probability or probabilities[i] > max_probability)):
+                    max_probability = probabilities[i]
+                    max_target = target
+
+        # ::TESTING
+        print(max_probability)
+        print(max_target)
+        if max_target: print(source.text); print(max_target.text)
+        # ::END
+
+        return max_target, max_probability

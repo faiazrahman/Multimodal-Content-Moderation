5a38666ca973a8a725efe40e14cd783439e2c93b
faiazrahman <faiaz.rahman@yale.edu>
2022-05-05 00:50:23 -0400

    keep v2 of BART ROUGE script (precompute summs then fetch in df.apply); only load Transformers pipeline if force or not already generated

--- dialogue_summarization/run_baseline_bart_rouge_evaluation.py
+++ dialogue_summarization/run_baseline_bart_rouge_evaluation.py
@@ -6,6 +6,7 @@ python -m dialogue_summarization.run_baseline_bart_rouge_evaluation
 """
 import os
 import logging
+from pprint import pprint
 from collections import defaultdict
 
 import pandas as pd
@@ -25,65 +26,6 @@ SAMSUM_VAL_DATAFRAME_PATH = os.path.join(SAMSUM_DATA_PATH, "samsum_val_dataframe
 
 logging.basicConfig(level=logging.DEBUG)
 
-## VERSION 1: COMPUTE SUMMARIES IN df.apply
-def _evaluate_baseline_bart(force_regenerate_summaries=False):
-    """ Evaluates baseline BART's summarization performance on SAMSum data """
-    rouge = PyRouge(
-        # ROUGE-N: Overlap of n-grams
-        rouge_n=(1, 2, 4),
-        # ROUGE-L: Longest common subsequence based statistics
-        rouge_l=True,
-        # ROUGE-W: Weighted LCS-based statistics that favors consecutive LCSs
-        rouge_w=True,
-        rouge_w_weight=1.2,
-        # ROUGE-S: Skip-bigram based co-occurrence statistics
-        # Note: A skip-bigram is any pair of words in their sentence order
-        rouge_s=True,
-        # ROUGE-SU: Skip-bigram plus unigram-based co-occurrence statistics
-        rouge_su=True,
-        skip_gap=4
-    )
-    logging.debug(rouge)
-
-    df = pd.read_pickle(SAMSUM_TEST_DATAFRAME_PATH)
-    logging.debug(df)
-
-    # Transformer pipeline to generate BART summaries
-    summarizer = transformers.pipeline("summarization", model="facebook/bart-large-cnn")
-
-    def generate_summary(row):
-        """ Generate summary of dialogue using BART Transformer pipeline """
-        dialogue_utterances = row['dialogue']
-        corpus = "\n".join(dialogue_utterances)
-
-        # We define the summary's max_length as max(min(75, num_words // 2), 5)
-        # Note that num_words is calculated very roughly, splitting on whitespace
-        num_words = sum([len(utterance.split()) for utterance in dialogue_utterances])
-        max_length = min(75, num_words // 2) # For short comment threads, it'll be <75
-        max_length = max(max_length, 5) # Avoid 1-length maxes, which leads to unexpected behavior
-        min_length = min(5, max_length - 1)
-        summary = summarizer(corpus, min_length=min_length, max_length=max_length, truncation=True)
-
-        # Pipeline returns a list containing a dict
-        # https://huggingface.co/docs/transformers/master/en/main_classes/pipelines
-        summary = summary[0]['summary_text']
-        return summary
-
-    # If baseline BART summaries have not been generated (i.e. not in the saved
-    # dataframe .pkl) or we're forcing re-generating summaries, run pipeline
-    if 'baseline_bart_summary' not in df.columns or force_regenerate_summaries:
-        # Generate the summaries and save them to the same dataframe .pkl
-        df['baseline_bart_summary'] = df.apply(lambda row: generate_summary(row), axis=1)
-        df.to_pickle(SAMSUM_TEST_DATAFRAME_PATH)
-
-    print(df)
-
-    hypotheses = df['baseline_bart_summary'].tolist()
-    references = df['summary'].tolist()
-    print(len(hypotheses), len(references))
-    # TODO: Run through ROUGE and get scores
-
-## VERSION 2: PRECOMPUTE ALL SUMMARIES THEN FETCH IN df.apply
 def evaluate_baseline_bart(force_regenerate_summaries=False):
     """ Evaluates baseline BART's summarization performance on SAMSum data """
     rouge = PyRouge(
@@ -107,7 +49,10 @@ def evaluate_baseline_bart(force_regenerate_summaries=False):
     logging.debug(df)
 
     # Transformer pipeline to generate BART summaries
-    summarizer = transformers.pipeline("summarization", model="facebook/bart-large-cnn")
+    # We initialize it here so it is in-scope for `generate_summary()`, but we
+    # do not load the model yet (since it may not be needed, if summaries have
+    # already been generated; this saves the time spent loading the model)
+    summarizer = None
 
     # Map id: summary (used to precompute summaries and then add them quickly
     # to the actual dataframe via `df.apply`)
@@ -150,6 +95,9 @@ def evaluate_baseline_bart(force_regenerate_summaries=False):
     # dataframe .pkl) or we're forcing re-generating summaries, run pipeline
     if 'baseline_bart_summary' not in df.columns or force_regenerate_summaries:
         logging.info("Generating baseline BART summaries for SAMSum (for ROUGE evaluation)...")
+        # Transformer pipeline to generate BART summaries
+        summarizer = transformers.pipeline("summarization", model="facebook/bart-large-cnn")
+
         # Generate the summaries and save them to the same dataframe .pkl
         for idx in range(len(df.index)):
             if idx % 50 == 0: print(f"Generating summaries for item {idx}...")
@@ -161,8 +109,13 @@ def evaluate_baseline_bart(force_regenerate_summaries=False):
 
     hypotheses = df['baseline_bart_summary'].tolist()
     references = df['summary'].tolist()
-    print(len(hypotheses), len(references))
-    # TODO: Run through ROUGE and get scores
+    if len(hypotheses) != len(references):
+        raise Exception(f"Error: The number of `baseline_bart_summary` strings ({len(hypotheses)}) does not match the number of reference `summary` strings ({len(references)}); Try re-running evaluate_baseline_bart() with force_regenerate_summaries=True")
+    print(f"\nNumber of summaries to evaluate: {len(hypotheses)}")
+
+    print("Evaluating baseline BART summaries on SAMSum with ROUGE...")
+    scores = rouge.evaluate(hypotheses, references)
+    pprint(scores)
 
 if __name__ == "__main__":
     evaluate_baseline_bart()
